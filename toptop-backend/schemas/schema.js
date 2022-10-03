9bb7728ddc8f7d449f1c64a7a5240fe869bf17ce
hieupm2k <hieupm@bap.jp>
2022-10-03 08:22:38 +0700

    revert code

--- toptop-backend/schemas/schema.js
+++ toptop-backend/schemas/schema.js
@@ -16,7 +16,6 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
-    //check
     user,
     post,
     comment,
